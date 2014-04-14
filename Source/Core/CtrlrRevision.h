e4a3cfdba4911d92b47220cdd08813e42e697caf
atom <atom@anus.local>
2014-04-14 22:42:30 +0200

    GIT fix for MAC

--- Source/Core/CtrlrRevision.h
+++ Source/Core/CtrlrRevision.h
@@ -1,7 +1,8 @@
-#ifndef __CTRLR_REVISION__ 
-#define __CTRLR_REVISION__ 
-/* Generated */ 
-static const char *ctrlrRevision              	= "899703535ed166df4b00c6b9be338b3691de00b6"; 
-static const char *ctrlrRevisionBuildDate   	= "Mon 04/14/2014"; 
-/* Generated */ 
-#endif 
+#ifndef __CTRLR_REVISION__
+#define __CTRLR_REVISION__
+
+static const char *ctrlrRevision              	= "262f271087f768fbfbe1c2a186518f69f686b938";
+static const char *ctrlrRevisionDate        	= "Mon Apr 14 22:16:19 2014 +0200";
+static const char *ctrlrRevisionBuildDate   	= "Mon Apr 14 22:34:08 CEST 2014";
+
+#endif
