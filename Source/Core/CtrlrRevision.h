ac6a31852a8e26ddbf6a7133f87f1fd6dac8a6c4
Roman Kubiak <kubiak.roman@gmail.com>
2014-04-23 14:30:50 +0200

    Fixed VST project binary path

--- Source/Core/CtrlrRevision.h
+++ Source/Core/CtrlrRevision.h
@@ -1,13 +1,7 @@
 #ifndef __CTRLR_REVISION__ 
 #define __CTRLR_REVISION__ 
-#define __CTRLR_REVISION__ 
 /* Generated */ 
-/* Generated */ 
-static const char *ctrlrRevision              	= "cd7853b04b80c1e47ca4a0f8aaf79f9572eb5ea3"; 
-static const char *ctrlrRevision              	= "cd7853b04b80c1e47ca4a0f8aaf79f9572eb5ea3"; 
-static const char *ctrlrRevisionBuildDate   	= "2014-04-23"; 
+static const char *ctrlrRevision              	= "218f9d4f3bc20376304f28e6738d664c69a0d617"; 
 static const char *ctrlrRevisionBuildDate   	= "2014-04-23"; 
 /* Generated */ 
-/* Generated */ 
-#endif 
 #endif 
