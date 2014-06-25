bb41e32370e477b25cb5ede9bb6493bd9dae09b5
RomanKubiak <kubiak.roman@gmail.com>
2014-06-25 23:44:19 +0200

    minor project changes

--- Scripts/win.bat
+++ Scripts/win.bat
@@ -1,22 +1,21 @@
 @ECHO OFF
 set TERM=msys
 set NSISDIR=C:\Program Files (x86)\NSIS
-set GIT="C:\Program Files (x86)\Git\bin\git.exe" "log" "-1"
+set GIT="C:\Program Files (x86)\Git\bin\git.exe" "describe" "--tags"
 
-%GIT% | find "commit" > REVISION
-FOR /f "tokens=1,2,3,4 delims=/ " %%a in (REVISION) do (set revision=%%b)
-
-echo Revision: %revision:~0,8%
+%GIT% > REVISION
+FOR /f %%a in (REVISION) do (set revision=%%a)
+echo Revision: %revision%
 
 echo Create installer using nsis in: "%NSISDIR%\makensis.exe"
 "%NSISDIR%\makensis.exe" installers/ctrlr.nsi /V4
 
 echo "Copy output to ctrlr.org"
-ren installers\Ctrlr.exe Ctrlr_%revision:~0,8%.exe
-pscp.exe -i private.ppk installers\Ctrlr_%revision:~0,8%.exe ctrlrorg@ctrlr.org:/home/ctrlrorg/public_html/nightly/
+ren installers\Ctrlr.exe Ctrlr_%revision%.exe
+pscp.exe -i private.ppk installers\Ctrlr_%revision%.exe ctrlrorg@ctrlr.org:/home/ctrlrorg/public_html/nightly/
 
 echo "Update changelog and revisions"
-plink.exe -v -i private.ppk ctrlrorg@ctrlr.org "nohup /home/ctrlrorg/crons/update_changelog.sh &"
-plink.exe -v -i private.ppk ctrlrorg@ctrlr.org "nohup /home/ctrlrorg/crons/update_revisions.sh &"
+REM plink.exe -v -i private.ppk ctrlrorg@ctrlr.org "nohup /home/ctrlrorg/crons/update_changelog.sh &"
+REM plink.exe -v -i private.ppk ctrlrorg@ctrlr.org "nohup /home/ctrlrorg/crons/update_revisions.sh &"
 
 del REVISION
