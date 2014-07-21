4b5a82b052b03d13330c8085e760fac98b93d091
RomanKubiak <kubiak.roman@gmail.com>
2014-07-21 23:35:49 +0200

    Fixed the Lua editor tabs to be more consistent. Fixed a small assertion when loading a panel (there was a performance counter for decompression that did nothing)

--- Source/UIComponents/CtrlrLua/MethodEditor/CtrlrLuaMethodEditorTabs.h
+++ Source/UIComponents/CtrlrLua/MethodEditor/CtrlrLuaMethodEditorTabs.h
@@ -5,15 +5,28 @@
 
 class CtrlrLuaMethodEditor;
 
+class CtrlrLuaMethodEditorTabsLF : public LookAndFeel_V3
+{
+	public:
+		CtrlrLuaMethodEditorTabsLF();
+		~CtrlrLuaMethodEditorTabsLF();
+		int	getTabButtonBestWidth (TabBarButton &, int tabDepth);
+		void drawTabButton (TabBarButton& button, Graphics& g, bool isMouseOver, bool isMouseDown);
+		static void createTabTextLayout (const TabBarButton& button, float length, float depth, Colour colour, TextLayout& textLayout);
+		Rectangle <int> getTabButtonExtraComponentBounds (const TabBarButton &, Rectangle< int > &textArea, Component &extraComp);
+};
+
 class CtrlrLuaMethodEditorTabs : public TabbedComponent
 {
 	public:
 		CtrlrLuaMethodEditorTabs(CtrlrLuaMethodEditor &_owner);
+		~CtrlrLuaMethodEditorTabs();
 		TabBarButton *createTabButton (const String &tabName, int tabIndex);
 		JUCE_LEAK_DETECTOR(CtrlrLuaMethodEditorTabs)
 
 	private:
 		CtrlrLuaMethodEditor &owner;
+		CtrlrLuaMethodEditorTabsLF *lf;
 };
 
 #endif
