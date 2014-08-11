1a0fd83f2c3c6c9199a8729457e01a02ac7b8bd6
RomanKubiak <kubiak.roman@gmail.com>
2014-08-11 23:01:50 +0200

    No changes

--- Juce/modules/juce_data_structures/juce_data_structures.cpp
+++ Juce/modules/juce_data_structures/juce_data_structures.cpp
@@ -33,6 +33,7 @@
 
 // Your project must contain an AppConfig.h file with your project-specific settings in it,
 // and your header search path must make it accessible to the module's files.
+#include "stdafx.h"
 #include "AppConfig.h"
 
 #include "juce_data_structures.h"
