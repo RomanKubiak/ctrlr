88600981c380fd116bc65cb50f60122b4efa34ff
RomanKubiak <kubiak.roman@gmail.com>
2014-08-11 23:01:50 +0200

    No changes

--- Juce/modules/juce_core/juce_core.cpp
+++ Juce/modules/juce_core/juce_core.cpp
@@ -37,6 +37,7 @@
 
 // Your project must contain an AppConfig.h file with your project-specific settings in it,
 // and your header search path must make it accessible to the module's files.
+#include "stdafx.h"
 #include "AppConfig.h"
 
 //==============================================================================
