bcfc54c413c43c6aa36c30ec8b2567af877f8dc2
RomanKubiak <kubiak.roman@gmail.com>
2014-12-02 15:55:33 +0100

    Added CtrlrRevision.h for people who want to just build. Updated JUCE to 3.1.0

---
+++ Source/Core/CtrlrRevision.h
@@ -0,0 +1,7 @@
+#ifndef __CTRLR_REVISION__
+#define __CTRLR_REVISION__
+
+static const char *ctrlrRevision              	= "6.6.6";
+static const char *ctrlrRevisionDate        	= "End of days";
+
+#endif
