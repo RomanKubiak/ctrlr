230f2c0471d23416aedf9cf6b339bc9f7bf58e07
RomanKubiak <kubiak.roman@gmail.com>
2014-12-02 15:55:33 +0100

    Added CtrlrRevision.h for people who want to just build. Updated JUCE to 3.1.0

--- Juce/modules/juce_opengl/utils/juce_OpenGLAppComponent.cpp
+++ Juce/modules/juce_opengl/utils/juce_OpenGLAppComponent.cpp
@@ -22,26 +22,42 @@
   ==============================================================================
 */
 
-#ifndef JUCE_AUDIO_UTILS_H_INCLUDED
-#define JUCE_AUDIO_UTILS_H_INCLUDED
-
-#include "../juce_gui_basics/juce_gui_basics.h"
-#include "../juce_audio_devices/juce_audio_devices.h"
-#include "../juce_audio_formats/juce_audio_formats.h"
-#include "../juce_audio_processors/juce_audio_processors.h"
-
-//=============================================================================
-namespace juce
+OpenGLAppComponent::OpenGLAppComponent()   : frameCounter (0)
 {
-
-#include "gui/juce_AudioDeviceSelectorComponent.h"
-#include "gui/juce_AudioThumbnailBase.h"
-#include "gui/juce_AudioThumbnail.h"
-#include "gui/juce_AudioThumbnailCache.h"
-#include "gui/juce_MidiKeyboardComponent.h"
-#include "gui/juce_AudioAppComponent.h"
-#include "players/juce_AudioProcessorPlayer.h"
-
+    setOpaque (true);
+    openGLContext.setRenderer (this);
+    openGLContext.attachTo (*this);
+    openGLContext.setContinuousRepainting (true);
 }
 
-#endif   // JUCE_AUDIO_UTILS_H_INCLUDED
+OpenGLAppComponent::~OpenGLAppComponent()
+{
+    // Before your subclass's destructor has completed, you must call
+    // shutdownOpenGL() to release the GL context. (Otherwise there's
+    // a danger that it may invoke a GL callback on your class while
+    // it's in the process of being deleted.
+    jassert (! openGLContext.isAttached());
+
+    shutdownOpenGL();
+}
+
+void OpenGLAppComponent::shutdownOpenGL()
+{
+    openGLContext.detach();
+}
+
+void OpenGLAppComponent::newOpenGLContextCreated()
+{
+    initialise();
+}
+
+void OpenGLAppComponent::renderOpenGL()
+{
+    ++frameCounter;
+    render();
+}
+
+void OpenGLAppComponent::openGLContextClosing()
+{
+    shutdown();
+}
