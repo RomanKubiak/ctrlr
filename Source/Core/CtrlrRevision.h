5767e685107325a47069d96ce3e5cc050eba1f5e
RomanKubiak <kubiak.roman@gmail.com>
2014-12-03 16:41:00 +0100

    Windows project update

--- Source/Core/CtrlrRevision.h
+++ Source/Core/CtrlrRevision.h
@@ -1,7 +1,7 @@
 #ifndef __CTRLR_REVISION__
 #define __CTRLR_REVISION__
 
-static const char *ctrlrRevision              	= "5.3.24";
-static const char *ctrlrRevisionDate        	= "Wed Dec  3 15:08:43 CET 2014";
+static const char *ctrlrRevision              	= "5.3.26";
+static const char *ctrlrRevisionDate        	= "Wed Dec  3 16:32:09 CEST 2014";
 
 #endif
