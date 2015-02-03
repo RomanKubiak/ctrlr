242ca52bce25f7addbd36dce6a3d1576892f5c2a
atom <atom@clitoris.moon.org>
2015-02-03 22:49:34 +0100

    Noexcept only for GCC (other platforms complain)

--- Source/Lua/luabind/luabind/config.hpp
+++ Source/Lua/luabind/luabind/config.hpp
@@ -36,6 +36,15 @@
 
 #define for if (false) {} else for
 
+// Is noexcept supported?
+#if defined(__clang__) && __has_feature(cxx_noexcept) || \
+defined(__GXX_EXPERIMENTAL_CXX0X__) && __GNUC__ * 10 + __GNUC_MINOR__ >= 46 || \
+defined(_MSC_FULL_VER) && _MSC_FULL_VER >= 180021114
+#  define NOEXCEPT(x) noexcept(x)
+#else
+#  define NOEXCEPT(x)
+#endif
+
 #include <cstring>
 
 namespace std
