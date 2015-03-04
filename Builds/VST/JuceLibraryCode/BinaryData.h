d893e356606ccc3e9ce966ac6445ca60d64763e9
RomanKubiak <kubiak.roman@gmail.com>
2015-03-04 11:51:21 +0100

    Windows project update

--- Builds/VST/JuceLibraryCode/BinaryData.h
+++ Builds/VST/JuceLibraryCode/BinaryData.h
@@ -232,7 +232,7 @@ namespace BinaryData
     const int            CtrlrMIDIVendors_xmlSize = 15061;
 
     extern const char*   CtrlrIDs_xml;
-    const int            CtrlrIDs_xmlSize = 81002;
+    const int            CtrlrIDs_xmlSize = 81076;
 
     extern const char*   CtrlrLuaMethodTemplates_xml;
     const int            CtrlrLuaMethodTemplates_xmlSize = 21208;
