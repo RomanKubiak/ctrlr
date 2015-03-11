75c035b28fcab279a6d8c07bd833271476345084
RomanKubiak <kubiak.roman@gmail.com>
2015-03-11 17:48:28 +0100

    Try to add some code to generate keyboard events from Ctrlr so you can control other apps with Ctrlr using QERTY keyboard.

---
+++ Source/Native/Linux/keys.cpp
@@ -0,0 +1,64 @@
+#ifdef LINUX
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#undef KeyPress
+#define KEYCODE XK_Down
+#include "keys.h"
+
+XKeyEvent createKeyEvent(Display *display, Window &win,
+                           Window &winRoot, bool press,
+                           int keycode, int modifiers)
+{
+   XKeyEvent event;
+
+   event.display     = display;
+   event.window      = win;
+   event.root        = winRoot;
+   event.subwindow   = None;
+   event.time        = CurrentTime;
+   event.x           = 1;
+   event.y           = 1;
+   event.x_root      = 1;
+   event.y_root      = 1;
+   event.same_screen = True;
+   event.keycode     = XKeysymToKeycode(display, keycode);
+   event.state       = modifiers;
+
+   if(press)
+      event.type = 2;
+   else
+      event.type = KeyRelease;
+
+   return event;
+}
+
+const Result ctrlr_sendKeyPressEvent (const KeyPress &keyPress)
+{
+    Display *display = XOpenDisplay(0);
+    if(display == NULL)
+        return (Result::fail("Can't open display"));
+
+   Window winRoot = XDefaultRootWindow(display);
+   Window winFocus;
+   int    revert;
+   if (!XGetInputFocus(display, &winFocus, &revert))
+   {
+       return (Result::fail("XGetInputFocus failed"));
+   }
+
+   XKeyEvent event = createKeyEvent(display, winFocus, winRoot, true, keyPress.getKeyCode(), keyPress.getModifiers().getRawFlags());
+   if (XSendEvent(event.display, event.window, True, KeyPressMask, (XEvent *)&event) == 0)
+   {
+       return (Result::fail("XSendEvent failed"));
+   }
+
+   event = createKeyEvent(display, winFocus, winRoot, false, keyPress.getKeyCode(), keyPress.getModifiers().getRawFlags());
+   if (XSendEvent(event.display, event.window, True, KeyPressMask, (XEvent *)&event) == 0)
+   {
+       return (Result::fail("XSendEvent failed"));
+   }
+
+   XCloseDisplay(display);
+   return (Result::ok());
+}
+#endif
