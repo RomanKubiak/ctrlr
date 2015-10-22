d069c68e400ebe673adf7e888211d3dd5a386d7b
r.kubiak <r.kubiak@samsung.com>
2015-10-22 13:19:48 +0200

    Updated plugin project for OSX (both standalone and plugin builds now depend on the native build SDK and target the 10.7 OSX version)

--- Builds/AU/JuceLibraryCode/BinaryData.h
+++ Builds/AU/JuceLibraryCode/BinaryData.h
@@ -196,10 +196,10 @@ namespace BinaryData
     const int            CtrlrMIDITransactions_xmlSize = 1963;
 
     extern const char*   CtrlrIDs_xml;
-    const int            CtrlrIDs_xmlSize = 81518;
+    const int            CtrlrIDs_xmlSize = 81525;
 
     extern const char*   CtrlrLuaMethodTemplates_xml;
-    const int            CtrlrLuaMethodTemplates_xmlSize = 21942;
+    const int            CtrlrLuaMethodTemplates_xmlSize = 21266;
 
     extern const char*   CtrlrMidiMultiTemplate_xml;
     const int            CtrlrMidiMultiTemplate_xmlSize = 662;
