3add64a23c85b3d9fafa3fa26199b001fdc67eaa
RomanKubiak <kubiak.roman@gmail.com>
2016-12-14 20:11:44 +0100

    Fallback to lua 5.1.4 due to not working exceptions luabind/boost remain updated

--- Source/Misc/include/compat.h
+++
@@ -1,8 +0,0 @@
-#include "lua.h"
-#include "lauxlib.h"
-
-
-int typeerror(lua_State* L, int narg, const char* tname);
-#define setfuncs luaL_setfuncs
-#define setuservalue lua_setuservalue
-#define getuservalue lua_getuservalue
