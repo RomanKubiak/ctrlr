b1194767f32681ba6d071325b1ad25e9149a4fae
Roman Kubiak <kubiak.roman@gmail.com>
2019-01-09 20:00:54 +0100

    6.0 version bump

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,423 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   FindLua52_cmake;
+    const int            FindLua52_cmakeSize = 4567;
+
+    extern const char*   FindLuabind_cmake;
+    const int            FindLuabind_cmakeSize = 1373;
+
+    extern const char*   adopt_rst;
+    const int            adopt_rstSize = 778;
+
+    extern const char*   copy_rst;
+    const int            copy_rstSize = 1055;
+
+    extern const char*   dependency_rst;
+    const int            dependency_rstSize = 1028;
+
+    extern const char*   discard_result_rst;
+    const int            discard_result_rstSize = 619;
+
+    extern const char*   out_value_rst;
+    const int            out_value_rstSize = 1605;
+
+    extern const char*   pure_out_value_rst;
+    const int            pure_out_value_rstSize = 1379;
+
+    extern const char*   raw_rst;
+    const int            raw_rstSize = 1155;
+
+    extern const char*   return_reference_to_rst;
+    const int            return_reference_to_rstSize = 1702;
+
+    extern const char*   return_stl_iterator_rst;
+    const int            return_stl_iterator_rstSize = 791;
+
+    extern const char*   yield_rst;
+    const int            yield_rstSize = 499;
+
+    extern const char*   acknowledgments_rst;
+    const int            acknowledgments_rstSize = 801;
+
+    extern const char*   basicusage_rst;
+    const int            basicusage_rstSize = 1856;
+
+    extern const char*   buildoptions_rst;
+    const int            buildoptions_rstSize = 5920;
+
+    extern const char*   building_rst;
+    const int            building_rstSize = 2028;
+
+    extern const char*   changes_txt;
+    const int            changes_txtSize = 5078;
+
+    extern const char*   classes_rst;
+    const int            classes_rstSize = 19994;
+
+    extern const char*   CMakeLists_txt;
+    const int            CMakeLists_txtSize = 2227;
+
+    extern const char*   conf_py;
+    const int            conf_pySize = 8495;
+
+    extern const char*   errors_rst;
+    const int            errors_rstSize = 7111;
+
+    extern const char*   exceptions_rst;
+    const int            exceptions_rstSize = 4572;
+
+    extern const char*   faq_rst;
+    const int            faq_rstSize = 4218;
+
+    extern const char*   functions_rst;
+    const int            functions_rstSize = 6697;
+
+    extern const char*   GetGitRevisionDescription_cmake;
+    const int            GetGitRevisionDescription_cmakeSize = 4172;
+
+    extern const char*   GetGitRevisionDescription_cmake_in;
+    const int            GetGitRevisionDescription_cmake_inSize = 1284;
+
+    extern const char*   git_version_in;
+    const int            git_version_inSize = 16;
+
+    extern const char*   implnotes_rst;
+    const int            implnotes_rstSize = 1820;
+
+    extern const char*   index_html;
+    const int            index_htmlSize = 2629;
+
+    extern const char*   index_rst;
+    const int            index_rstSize = 983;
+
+    extern const char*   intro_rst;
+    const int            intro_rstSize = 2708;
+
+    extern const char*   issues_rst;
+    const int            issues_rstSize = 579;
+
+    extern const char*   luaclasses_rst;
+    const int            luaclasses_rstSize = 10753;
+
+    extern const char*   luabindlogolabel_ps;
+    const int            luabindlogolabel_psSize = 5047;
+
+    extern const char*   luabind_png;
+    const int            luabind_pngSize = 20263;
+
+    extern const char*   make_bat;
+    const int            make_batSize = 7150;
+
+    extern const char*   Makefile;
+    const int            MakefileSize = 6943;
+
+    extern const char*   object_rst;
+    const int            object_rstSize = 11199;
+
+    extern const char*   policies_rst;
+    const int            policies_rstSize = 9932;
+
+    extern const char*   scopes_rst;
+    const int            scopes_rstSize = 1990;
+
+    extern const char*   splitregistration_rst;
+    const int            splitregistration_rstSize = 833;
+
+    extern const char*   style_css;
+    const int            style_cssSize = 5306;
+
+    extern const char*   userdefinedconverters_rst;
+    const int            userdefinedconverters_rstSize = 1515;
+
+    extern const char*   CMakeLists_txt2;
+    const int            CMakeLists_txt2Size = 5694;
+
+    extern const char*   CMakeLists_txt3;
+    const int            CMakeLists_txt3Size = 9283;
+
+    extern const char*   INSTALL_txt;
+    const int            INSTALL_txtSize = 2121;
+
+    extern const char*   LICENSE_txt;
+    const int            LICENSE_txtSize = 1230;
+
+    extern const char*   README_md;
+    const int            README_mdSize = 7480;
+
+    extern const char*   CtrlrRevision_template;
+    const int            CtrlrRevision_templateSize = 199;
+
+    extern const char*   FONT_60sekuntia_ttf;
+    const int            FONT_60sekuntia_ttfSize = 97264;
+
+    extern const char*   FONT_Computerfont_ttf;
+    const int            FONT_Computerfont_ttfSize = 39104;
+
+    extern const char*   FONT_Digit_ttf;
+    const int            FONT_Digit_ttfSize = 13028;
+
+    extern const char*   FONT_Digital7_ttf;
+    const int            FONT_Digital7_ttfSize = 34360;
+
+    extern const char*   FONT_DottyShadow_ttf;
+    const int            FONT_DottyShadow_ttfSize = 569464;
+
+    extern const char*   FONT_Electronic_Highway_Sign_ttf;
+    const int            FONT_Electronic_Highway_Sign_ttfSize = 73060;
+
+    extern const char*   FONT_Invasion2000_ttf;
+    const int            FONT_Invasion2000_ttfSize = 30836;
+
+    extern const char*   FONT_Karmatic_Arcade_ttf;
+    const int            FONT_Karmatic_Arcade_ttfSize = 56908;
+
+    extern const char*   FONT_LCD_ttf;
+    const int            FONT_LCD_ttfSize = 27664;
+
+    extern const char*   FONT_WarenhausStandard_ttf;
+    const int            FONT_WarenhausStandard_ttfSize = 28212;
+
+    extern const char*   FONT_ZX81_ttf;
+    const int            FONT_ZX81_ttfSize = 17176;
+
+    extern const char*   appbar_control_play_png;
+    const int            appbar_control_play_pngSize = 434;
+
+    extern const char*   appbar_debug_clean_png;
+    const int            appbar_debug_clean_pngSize = 723;
+
+    extern const char*   appbar_debug_restart_png;
+    const int            appbar_debug_restart_pngSize = 436;
+
+    extern const char*   appbar_debug_step_into_png;
+    const int            appbar_debug_step_into_pngSize = 492;
+
+    extern const char*   appbar_debug_step_out_png;
+    const int            appbar_debug_step_out_pngSize = 469;
+
+    extern const char*   appbar_debug_step_over_png;
+    const int            appbar_debug_step_over_pngSize = 489;
+
+    extern const char*   appbar_debug_stop_png;
+    const int            appbar_debug_stop_pngSize = 218;
+
+    extern const char*   ico_box_png;
+    const int            ico_box_pngSize = 3869;
+
+    extern const char*   ico_bug_png;
+    const int            ico_bug_pngSize = 4091;
+
+    extern const char*   ico_copy_png;
+    const int            ico_copy_pngSize = 3721;
+
+    extern const char*   ico_ctrlr_mac_icns;
+    const int            ico_ctrlr_mac_icnsSize = 64064;
+
+    extern const char*   ico_cut_png;
+    const int            ico_cut_pngSize = 4206;
+
+    extern const char*   ico_delete_png;
+    const int            ico_delete_pngSize = 3644;
+
+    extern const char*   ico_document_png;
+    const int            ico_document_pngSize = 3303;
+
+    extern const char*   ico_document_new_png;
+    const int            ico_document_new_pngSize = 3841;
+
+    extern const char*   ico_edit_document_png;
+    const int            ico_edit_document_pngSize = 3781;
+
+    extern const char*   ico_eye_png;
+    const int            ico_eye_pngSize = 3744;
+
+    extern const char*   ico_file_png;
+    const int            ico_file_pngSize = 3198;
+
+    extern const char*   ico_firmware_png;
+    const int            ico_firmware_pngSize = 269;
+
+    extern const char*   ico_folder_png;
+    const int            ico_folder_pngSize = 3300;
+
+    extern const char*   ico_folder_alt_png;
+    const int            ico_folder_alt_pngSize = 367;
+
+    extern const char*   ico_folder_new_png;
+    const int            ico_folder_new_pngSize = 1738;
+
+    extern const char*   ico_font_bold_png;
+    const int            ico_font_bold_pngSize = 3436;
+
+    extern const char*   ico_font_italic_png;
+    const int            ico_font_italic_pngSize = 3323;
+
+    extern const char*   ico_font_underline_png;
+    const int            ico_font_underline_pngSize = 3364;
+
+    extern const char*   ico_gui_png;
+    const int            ico_gui_pngSize = 3311;
+
+    extern const char*   ico_happy_png;
+    const int            ico_happy_pngSize = 4371;
+
+    extern const char*   ico_import_document_png;
+    const int            ico_import_document_pngSize = 3600;
+
+    extern const char*   ico_midi_png;
+    const int            ico_midi_pngSize = 16903;
+
+    extern const char*   ico_midi_plug_png;
+    const int            ico_midi_plug_pngSize = 1401;
+
+    extern const char*   ico_midi_plug_in_png;
+    const int            ico_midi_plug_in_pngSize = 1172;
+
+    extern const char*   ico_midi_plug_out_png;
+    const int            ico_midi_plug_out_pngSize = 1208;
+
+    extern const char*   ico_midi_small_png;
+    const int            ico_midi_small_pngSize = 6920;
+
+    extern const char*   ico_new_document_png;
+    const int            ico_new_document_pngSize = 3493;
+
+    extern const char*   ico_next_png;
+    const int            ico_next_pngSize = 4099;
+
+    extern const char*   ico_paste_png;
+    const int            ico_paste_pngSize = 3739;
+
+    extern const char*   ico_previous_png;
+    const int            ico_previous_pngSize = 4163;
+
+    extern const char*   ico_receive_png;
+    const int            ico_receive_pngSize = 3359;
+
+    extern const char*   ico_sad_png;
+    const int            ico_sad_pngSize = 4314;
+
+    extern const char*   ico_send_png;
+    const int            ico_send_pngSize = 3420;
+
+    extern const char*   ico_snapshot_png;
+    const int            ico_snapshot_pngSize = 1108;
+
+    extern const char*   ico_snapshot_new_png;
+    const int            ico_snapshot_new_pngSize = 1897;
+
+    extern const char*   ico_transaction_png;
+    const int            ico_transaction_pngSize = 3798;
+
+    extern const char*   ico_unit_png;
+    const int            ico_unit_pngSize = 4223;
+
+    extern const char*   ico_unit_alt_png;
+    const int            ico_unit_alt_pngSize = 1157;
+
+    extern const char*   ico_unknown_png;
+    const int            ico_unknown_pngSize = 3086;
+
+    extern const char*   ico_waveform_png;
+    const int            ico_waveform_pngSize = 1520;
+
+    extern const char*   tile_gif;
+    const int            tile_gifSize = 326;
+
+    extern const char*   debugger_lua;
+    const int            debugger_luaSize = 44867;
+
+    extern const char*   LICENSE;
+    const int            LICENSESize = 1099;
+
+    extern const char*   README_md2;
+    const int            README_md2Size = 272;
+
+    extern const char*   test_lua;
+    const int            test_luaSize = 77;
+
+    extern const char*   json_lua;
+    const int            json_luaSize = 17174;
+
+    extern const char*   LICENCE_txt;
+    const int            LICENCE_txtSize = 1097;
+
+    extern const char*   gen_LLookAndFeel_cpp_sh;
+    const int            gen_LLookAndFeel_cpp_shSize = 2671;
+
+    extern const char*   gen_LLookAndFeel_h_sh;
+    const int            gen_LLookAndFeel_h_shSize = 4001;
+
+    extern const char*   gen_LookAndFeel_lua_sh;
+    const int            gen_LookAndFeel_lua_shSize = 2311;
+
+    extern const char*   gen_LookAndFeelBase_cpp_sh;
+    const int            gen_LookAndFeelBase_cpp_shSize = 3269;
+
+    extern const char*   gen_LookAndFeelBase_h_sh;
+    const int            gen_LookAndFeelBase_h_shSize = 2074;
+
+    extern const char*   gen_LookAndFeelParamWrapperLuaMemberBinding_sh;
+    const int            gen_LookAndFeelParamWrapperLuaMemberBinding_shSize = 556;
+
+    extern const char*   gen_LookAndFeelParamWrapper_h_sh;
+    const int            gen_LookAndFeelParamWrapper_h_shSize = 8559;
+
+    extern const char*   inspect_lua;
+    const int            inspect_luaSize = 9563;
+
+    extern const char*   lf_template;
+    const int            lf_templateSize = 13631;
+
+    extern const char*   LookAndFeel_lua;
+    const int            LookAndFeel_luaSize = 15147;
+
+    extern const char*   paramWrapper_template;
+    const int            paramWrapper_templateSize = 3520;
+
+    extern const char*   RSRC_zip;
+    const int            RSRC_zipSize = 49800;
+
+    extern const char*   CtrlrIDs_xml;
+    const int            CtrlrIDs_xmlSize = 83121;
+
+    extern const char*   CtrlrLuaClassTemplates_xml;
+    const int            CtrlrLuaClassTemplates_xmlSize = 109;
+
+    extern const char*   CtrlrLuaMethodTemplates_xml;
+    const int            CtrlrLuaMethodTemplates_xmlSize = 21578;
+
+    extern const char*   CtrlrMidiMultiTemplate_xml;
+    const int            CtrlrMidiMultiTemplate_xmlSize = 662;
+
+    extern const char*   CtrlrMIDITransactions_xml;
+    const int            CtrlrMIDITransactions_xmlSize = 1963;
+
+    extern const char*   CtrlrMIDIVendors_xml;
+    const int            CtrlrMIDIVendors_xmlSize = 15061;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 132;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
