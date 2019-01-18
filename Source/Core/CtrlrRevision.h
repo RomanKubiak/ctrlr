70fbc4b3fe9859326388c242fd7225d55b248430
RomanKubiak <kubiak.roman@gmail.com>
2019-01-18 15:17:14 +0100

    JUCE removed as module

--- Source/Core/CtrlrRevision.h
+++ Source/Core/CtrlrRevision.h
@@ -1,7 +1,7 @@
 #ifndef __CTRLR_REVISION__
 #define __CTRLR_REVISION__
 
-static const char *ctrlrRevision              	= "6.0.1";
-static const char *ctrlrRevisionDate        	= "Wed, Jan 09, 2019  8:34:54 PM";
+static const char *ctrlrRevision              	= "6.0.4";
+static const char *ctrlrRevisionDate        	= "Wed, Jan 16, 2019  6:20:08 PM";
 
 #endif
