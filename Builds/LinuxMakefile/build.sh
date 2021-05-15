76d63daa903da4ce1c5dd22f60147b699d38462f
Candidus <candidus@xmail.net>
2021-05-15 20:10:06 +0200

    Detect libbfd on RPM-based Linux distributions

--- Builds/LinuxMakefile/build.sh
+++ Builds/LinuxMakefile/build.sh
@@ -3,8 +3,18 @@ MAXJOBS=`nproc`
 echo "CTRLR[linux]: Building for $HOSTTYPE, JOBS $MAXJOBS"
 CFLAGS=""
 
-if $(dpkg --compare-versions $(dpkg-query -f='${Version}' --show binutils-dev) gt 2.34); then
-	CFLAGS="-DHAVE_BFD_2_34=1"
+if command -v dpkg >/dev/null; then
+	echo "dpkg found"
+	if $(dpkg --compare-versions $(dpkg-query -f='${Version}' --show binutils-dev) gt 2.34); then
+		echo "libbfd 2.34 or newer found"
+		CFLAGS="-DHAVE_BFD_2_34=1"
+	fi
+elif command -v rpm >/dev/null; then
+	echo "rpm found"
+	if rpm -q --qf '%{Version}' binutils-devel | awk -F . '{ exit ($1 >= 2 && $2 >= 34) ? 0 : 1 }'; then
+		echo "libbfd 2.34 or newer found"
+		CFLAGS="-DHAVE_BFD_2_34=1"
+	fi
 fi
 
 eval CFLAGS=$CFLAGS CC=clang CXX=clang++ make V=1 CONFIG=Release -j$MAXJOBS "$@"
